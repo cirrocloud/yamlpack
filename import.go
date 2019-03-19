b5adf51cd45298a6e387a5493a2d25759f52ccce
Dan Taylor <C-Dan.Taylor@charter.com>
2019-03-19 12:50:32 -0600

    extend to run filters, apply template to chunks

--- import.go
+++ import.go
@@ -70,6 +70,21 @@ func (yp *Yp) Import(s string, r io.Reader) error {
 	return nil
 }
 
+//ImportWithTemplateFuncAndFilter takes a location identifier (URI, file path, etc..), io.Reader, a templating function, and a filter function
+//imported data is added to the yamlPack instance
+func (yp *Yp) WithTemplateFuncAndFilter(s string, r io.Reader, tmplFunc TemplateFunc) error {
+	yf, err := importYamlWithTemplateFunc(r, tmplFunc)
+	if err != nil {
+		return err
+	}
+	yp.Lock()
+	defer func() {
+		yp.Unlock()
+	}()
+	yp.Files[s] = yf
+	return nil
+}
+
 //Import takes a location identifier (URI, file path, etc..) and an io.Reader
 //imported data is added to the yamlPack instance
 func (yp *Yp) ImportWithTemplateFunc(s string, r io.Reader, tmplFunc TemplateFunc) error {
@@ -86,18 +101,35 @@ func (yp *Yp) ImportWithTemplateFunc(s string, r io.Reader, tmplFunc TemplateFun
 }
 
 func importYamlWithTemplateFunc(r io.Reader, tmplFunc TemplateFunc) ([]*YamlSection, error) {
+	return importYamlWithTemplateFuncAndFilters(r, tmplFunc, []string{".+"})
+}
+
+//ImportWithTemplateFuncAndFilters takes a location identifier (URI, file path, etc..) and an io.Reader, template function, and filter string array (regexp)
+//imported data is added to the yamlPack instance
+//sections within that do not match any of the supplied regexp filters will be ignored
+//the template function will be run on all sections that pass the filter before the section is added to the *Yp instance
+func (yp *Yp) ImportWithTemplateFuncAndFilters(s string, r io.Reader, tmplFunc TemplateFunc, filters []string) error {
+	yf, err := importYamlWithTemplateFuncAndFilters(r, tmplFunc, filters)
+	if err != nil {
+		return err
+	}
+	yp.Lock()
+	defer func() {
+		yp.Unlock()
+	}()
+	yp.Files[s] = yf
+	return nil
+}
+
+func importYamlWithTemplateFuncAndFilters(r io.Reader, tmplFunc TemplateFunc, filters []string) ([]*YamlSection, error) {
 	sections := []*YamlSection{}
 	buf := new(bytes.Buffer)
 	_, err := buf.ReadFrom(r)
 	if err != nil {
 		return nil, fmt.Errorf(fmt.Sprintf("could not read data %v", err))
 	}
-	data := buf.Bytes()
 
-	data, err = tmplFunc(data)
-	if err != nil {
-		return nil, err
-	}
+	data := buf.Bytes()
 
 	rxChunks := regexp.MustCompile(`---`)
 	chunks := rxChunks.FindAllIndex(data, -1)
@@ -109,54 +141,57 @@ func importYamlWithTemplateFunc(r io.Reader, tmplFunc TemplateFunc) ([]*YamlSect
 		}}
 	}
 	for i := range chunks {
-		vp := viper.New()
-		vp.SetConfigType("yaml")
+		//Each chunk is a document, needs to be filtered so the consumer gets the sections they want
+		// then run the supplied template, then Vipered
+		// this avoids running templates on unrelated sections that we may not have the data for
+
+		//extract section
 		var b []byte
 		if i < len(chunks)-1 {
 			b = data[chunks[i][1]:chunks[i+1][0]]
 		} else {
 			b = data[chunks[i][1]:]
 		}
+
+		//run filters
+		if !func() bool {
+			for _, v := range filters {
+				rx := regexp.MustCompile(v)
+				scanner := bufio.NewScanner(bytes.NewBuffer(b))
+				for scanner.Scan() {
+					if rx.MatchString(scanner.Text()) {
+						return true
+					}
+				}
+			}
+			return false
+		}() {
+			continue
+		}
+
+		//run template
+		b, err = tmplFunc(b)
+		if err != nil {
+			return nil, err
+		}
+
+		//add viper
+		vp := viper.New()
+		vp.SetConfigType("yaml")
 		if err := vp.ReadConfig(bytes.NewBuffer(b)); err != nil {
 			fmt.Printf("---\n%v\n", string(b))
 			return nil, errors.Wrap(err, "failed to import yaml section")
 		}
+
+		//save completed section
 		sections = append(sections, &YamlSection{Bytes: b, Viper: vp})
 	}
 	return sections, nil
 }
 
 func importYaml(r io.Reader) ([]*YamlSection, error) {
-	sections := []*YamlSection{}
-	buf := new(bytes.Buffer)
-	_, err := buf.ReadFrom(r)
-	if err != nil {
-		return nil, fmt.Errorf(fmt.Sprintf("could not read data %v", err))
+	templateFunc := func(b []byte) ([]byte, error) {
+		return b, nil
 	}
-	data := buf.Bytes()
-	rxChunks := regexp.MustCompile(`---`)
-	chunks := rxChunks.FindAllIndex(data, -1)
-	if len(chunks) == 0 {
-		//This is missing the end delimiter ('---') or both,
-		//either way we are treating the whole file as 1 section
-		chunks = [][]int{[]int{
-			int(0), int(len(data)),
-		}}
-	}
-	for i := range chunks {
-		vp := viper.New()
-		vp.SetConfigType("yaml")
-		var b []byte
-		if i < len(chunks)-1 {
-			b = data[chunks[i][1]:chunks[i+1][0]]
-		} else {
-			b = data[chunks[i][1]:]
-		}
-		if err := vp.ReadConfig(bytes.NewBuffer(b)); err != nil {
-			fmt.Printf("---\n%v\n", string(b))
-			return nil, errors.Wrap(err, "failed to import yaml section")
-		}
-		sections = append(sections, &YamlSection{Bytes: b, Viper: vp})
-	}
-	return sections, nil
+	return importYamlWithTemplateFunc(r, templateFunc)
 }
